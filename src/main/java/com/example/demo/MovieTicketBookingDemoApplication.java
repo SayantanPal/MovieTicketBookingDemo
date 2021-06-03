32d2e267908a9b7dba25df72ec546b89508eb62c
Sayantan Pal <sayantan.pal01@sap.com>
2021-06-03 21:07:32 +0530

    bug fixed

--- src/main/java/com/example/demo/MovieTicketBookingDemoApplication.java
+++ src/main/java/com/example/demo/MovieTicketBookingDemoApplication.java
@@ -3,14 +3,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages={"com.*"})
-public class MovieTicketBookingDemoApplication //extends SpringBootServletInitializer
+public class MovieTicketBookingDemoApplication extends SpringBootServletInitializer
 {//Change Class name
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieTicketBookingDemoApplication.class, args);//Change Class name
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(MovieTicketBookingDemoApplication.class);
+	}
+	
 
 }
